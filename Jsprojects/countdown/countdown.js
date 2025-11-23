543847c8a3ec71620ee02e43102874a55cb3de47
pradeep <ps5712172@gmail.com>
2025-11-23 10:22:25 +0530

    colorgenerator

--- Jsprojects/countdown/countdown.js
+++ Jsprojects/countdown/countdown.js
@@ -1,4 +1,4 @@
-const endDate = "22 November 2025 1:54:00 PM";
+const endDate = "23 November 2025 1:54:00 PM";
 document.getElementById("end-date").innerText = endDate;
 
 const inputs = document.querySelectorAll(".col input[readonly]");
